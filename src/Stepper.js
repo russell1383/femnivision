f4fb892adc1726bfac0727f51947efd37c51ee18
Russell Abdullah Al Mamun <joy.dtech@gmail.com>
2023-01-18 23:41:39 +0600

    Update Stepper.js

--- src/Stepper.js
+++ src/Stepper.js
@@ -12,22 +12,26 @@ const Stepper = () => {
   <li class="c-stepper__item"><h1 data-aos="fade-right"   class="c-stepper__title"> Step 1</h1>
         <div  data-aos="fade-left" class="c-stepper__content">
             
-            <h2 class='c-stepper__package-name'>Free No-Obligation 20-minute Discovery Call</h2>
-            <p class='c-stepper__package-desc'>No obligation 20 minutes consultation is available for clients. This will allow us to find out if we are the right fit for each other. You may ask questions to have an idea about my services. I will find out about your pain points to provide the right solution for you. If it is not a good fit, I will point you in the right direction. If we are a good fit to embark on working together, I will send you consent and intake forms which need to be signed and returned with your payment before scheduling a session with me.</p>
+            <h2 class='c-stepper__package-name'>Free No-Obligation 20-Minute Discovery Call</h2>
+            <p class='c-stepper__package-desc'>To establish if we are the right fit for each other.  I offer a free 20-minute video conversation over Zoom.  I will use this time to find out more about you and ask questions to see if I can help you with your issue.  If it isn’t a good fit, I will let you know and point you in the right direction.  Once we have established that we will work together, I will send you a Client Intake Form and my Terms and Conditions, which you will need to sign and return with your payment before scheduling a session with me.</p>
         </div>
     </li>
     <li class="c-stepper__item"><h1 data-aos="fade-right"   class="c-stepper__title">Step 2</h1>
         <div data-aos="fade-left" class="c-stepper__content">
             
             <h2 class='c-stepper__package-name'>Hypnotherapy Session Online</h2>
-            <p class='c-stepper__package-desc'>The first session which lasts for 1 hour, involves gathering information and explaining how the mind works. While taking the time to know and understand you, I will focus on your strengths and resilience, and build a relationship with you. Consequent sessions are focused on exploring solutions and applying hypnosis. Each session is productively engaging so you can walk away with a sense of relief, peace, and knowledge for your support. A confidential space is offered to explore the thoughts, feelings, and challenges in your life and help you find confidence, happiness, and peace by equipping you with the tools you need to manage your emotions and know yourself. I use creative techniques, tailoring your needs and personality focusing on brain function and positivity.</p>
+            <p class='c-stepper__package-desc'>The session will be over the internet and last approximately 1.5 to 2 hours.  If we need a little extra time, I always ask clients to block out three hours.  During the initial 15-minute consultations, we will review the information you gave me and clarify exactly what you want to do and how you want to be.  This will be followed by an RTT hypnotherapy session, which includes hypnosis and regression to find the root cause of the issue.  Your session will be completed with a reframing and powerful transformation that aims at installing a positive new suggestion into your subconscious mind.</p>
         </div>
     </li>
     <li class="c-stepper__item"><h1 data-aos="fade-right"   class="c-stepper__title">Step 3</h1>
         <div data-aos="fade-left" class="c-stepper__content">
             
-            <h2 class='c-stepper__package-name'>An Audio of Hypnosis</h2>
-            <p class='c-stepper__package-desc'>After our first session, I will send a voice recording that you listen to daily along with our sessions. I will send this recording to you via a preferred media so you can download the recording onto your mobile phone or computer for daily listening. The success of your investment depends on the fact that you listen to your recording daily preferably before you go to sleep and whenever you think it is necessary during the day. Repeated listening to your recording will allow the new, positive, and supporting beliefs to be firmly installed. Doing so will break those old unwanted habits and create new neurological pathways, rewiring the brain with new positive suggestions.
+            <h2 class='c-stepper__package-name'>Hypnotherapy Voice Recording + Optional Video                       
+		Deepener</h2>
+            <p class='c-stepper__package-desc'>After our session, I will produce a voice recording that you listen to daily for a minimum of 21 days following your session.  I will send this recording to you via email so you can download the recording onto your mobile phone or computer for daily listening.
+The success of your investment depends on the fact that you listen to your recording daily for at least 21 days following your session.  Repeated listening to your recording will allow the new, positive and supporting beliefs to be firmly installed.  Doing so will break those old unwanted habits and create new neurological pathways, rewiring the brain.
+A custom Video Deepener is offered as an add-on.  The video deepener comprises a hypnotic video animation, my suggestive voice and meditative background music.  The video animation has been designed in such a way that it will allow my evocative voice to reach the deeper levels of your subconscious mind while your conscious mind is drawn in and fascinated by the hypnotic video animation.
+
 
 </p>
         </div>
@@ -36,7 +40,7 @@ const Stepper = () => {
         <div data-aos="fade-left" class="c-stepper__content">
           
             <h2 class='c-stepper__package-name'>Post-Session Follow-Up</h2>
-            <p class='c-stepper__package-desc'>The success of your investment depends on the fact that you work according to our agreed plan which is made during the session and also listen to your recording daily. I will be in touch with you ten days after your session to support you. If needed we will schedule a check-in call.
+            <p class='c-stepper__package-desc'>The success of your investment depends on the fact that you listen to your recording daily for a minimum of 21 days following your RTT hypnotherapy session.  I will be with you 10 days after your session to support you.  After 21 days of listening to the recording, we will schedule a check-in call.
 
 
 </p>
